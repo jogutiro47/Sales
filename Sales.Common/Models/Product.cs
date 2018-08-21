e1816be0058dd8024e7a6135bf12b72719609977
jogutiro47 <jogutiro@gmail.com>
2018-08-21 13:42:05 -0500

    Clase 4

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Product
+    {
+        [Key]
+        //Agregar referencia data.anottations a Key
+        
+        public int ProducId { get; set; }
+
+        [Required]
+        public string Descripction { get; set; }
+        public Decimal Price { get; set; }
+
+        public bool IsAvalaible { get; set; }
+
+        public DateTime PublishOn { get; set; }
+    }
+}
