e5a152551e92fd2f63798e1193853a180d5f14ae
jogu47 <jogutiro@gmail.com>
2018-08-22 17:19:33 -0500

    ApiServices2

---
+++ Sales/Sales/Services/ApiService.cs
@@ -0,0 +1,27 @@
+﻿namespace Sales.Services
+{
+    using Sales.Common.Models;
+    using System;
+    using System.Threading.Tasks;
+
+    public class ApiService
+    {
+        public async Task<Response> GetList<T>(string urlbase, string prefix, string controller)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+
+                };
+            }
+        }
+    }
+}
